b827ddf03426c7d6f565598f4e71773dc1a7d637
jebritash-tech <jebritash@gmail.com>
2026-07-11 08:52:22 +0300

    Create sounds.js

---
+++ js/sounds.js
@@ -0,0 +1,14 @@
+const sounds = {
+    success: new Audio('assets/sounds/success.mp3'),
+    error: new Audio('assets/sounds/error.mp3'),
+    checkout: new Audio('assets/sounds/checkout.mp3'),
+    sync: new Audio('assets/sounds/sync.mp3')
+};
+
+function playSound(type) {
+    const sound = sounds[type];
+    if (!sound) return;
+
+    sound.currentTime = 0;
+    sound.play().catch(() => {});
+}
