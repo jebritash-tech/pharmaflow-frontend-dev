f513737f5423236628715f92c11987ad28ab1baf
jebritash-tech <jebritash@gmail.com>
2026-08-08 13:39:30 +0300

    Update branch.service.js

--- js/services/branch.service.js
+++ js/services/branch.service.js
@@ -4,7 +4,7 @@ const BranchService = {
 
         const res = await axios.get(
 
-            "https://pharmaflow-api-1.1.0-beta-main.test/api" + "/branches"
+            "https://pharmaflow-api-2-0-0-stable.onrender.com/api" + "/branches"
 
         );
 
@@ -24,7 +24,7 @@ const BranchService = {
 
         const res = await axios.get(
 
-            "https://pharmaflow-api-1.1.0-beta-main.test/api" + "/branches/" + id
+            "https://pharmaflow-api-2-0-0-stable.onrender.com/api" + "/branches/" + id
 
         );
 
